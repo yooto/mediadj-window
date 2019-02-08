48ad876e26e22b5574ac6394660fca5883ce8084
池原優斗 <ikeharayuuto@ikeharayuutonoMacBook-Pro.local>
2019-02-08 16:59:58 +0900

    commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var socketio = require('socket.io');
+var fs = require('fs');
+
+// 送受信された記録を管理する変数
+let stateArray = [0,0,0,0];
+
+
+var server = http.createServer(function(req, res){
+    // 現在のデータの状態を送信する
+    io.sockets.emit('server_to_client', stateArray);
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.end(fs.readFileSync('index.html', 'utf-8'));
+}).listen(process.env.PORT || 3000);
+
+var io = socketio.listen(server);
+
+io.sockets.on('connection', function(socket){
+    socket.on('client_to_server', function(data){
+        // 受信時の処理
+        if(data === 1){
+            stateArray[0] += 1;
+        }else if(data === 2){
+            stateArray[1] += 1;
+        }else if(data === 3){
+            stateArray[2] += 1;
+        }else if(data === 4){
+            stateArray[3] += 1;
+        }else if(data === "reset"){
+            stateArray[0] = 0;
+            stateArray[1] = 0;
+            stateArray[2] = 0;
+            stateArray[3] = 0;
+        }
+
+        io.sockets.emit('server_to_client', stateArray);
+    });
+});
